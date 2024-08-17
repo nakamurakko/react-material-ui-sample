90d2d2b2abc03f965fa4c43dcc7c4f153bf65e25
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2024-08-17 09:24:36 +0900

    web-vitals を削除。

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ import { Router } from '@remix-run/router';
 import App from './App';
 import ButtonSample from './button-sample/button-sample';
 import CheckboxSample from './checkbox-sample/checkbox-sample';
-import reportWebVitals from './reportWebVitals';
 
 const router: Router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,8 +27,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
